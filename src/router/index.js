0be4775d3179c5ad5db5ae3458c1af6946a8af2f
fxxsj <i@fxxsj.xyz>
2021-06-11 16:06:45 +0800

    4.5. 完善登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,16 @@ const routes = [
     path: '/auth/register',
     name: 'Register',
     component: () => import('@/views/auth/Register')
+  },
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/Home')
+  },
+  {
+    path: '*',
+    // 重定向
+    redirect: '/'
   }
 ]
 
@@ -15,5 +25,16 @@ const router = new Router({
   mode: 'history',
   routes
 })
+
+router.beforeEach((to, from, next) => {
+    const auth = router.app.$options.store.state.auth
+  
+    if (auth && to.path.indexOf('/auth/') !== -1) {
+      next('/')
+    } else {
+      next()
+    }
+  })
+  
 
 export default router
