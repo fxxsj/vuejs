476d0b23cebdd253e9227119b8466cf5feb07452
fxxsj <i@fxxsj.xyz>
2021-06-11 15:58:43 +0800

    4.4. 完善注册功能

--- src/main.js
+++ src/main.js
@@ -3,10 +3,14 @@ import App from './App'
 import router from './router'
 import './directives'
 import './components'
+// 引入 store/index.js 的默认值
+import store from './store'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  // 注入 store
+  store,
   render: h => h(App),
 }).$mount('#app')
